865abfe2b28600837703277920dfe9f6968511cf
Aleksey Averianov <Flex_d1@mail.ru>
2025-04-30 11:10:45 +0300

    комментарий 3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-  import { ref } from 'vue';
-  const date = ref(new Date().toLocaleString())
+
 </script>
 
 <template>
-  <div>
-    {{ date }}
-  </div>
+  
 </template>
 
 <style scoped></style>
