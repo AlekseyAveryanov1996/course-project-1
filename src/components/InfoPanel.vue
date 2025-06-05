7e7f58067581872786cf7210ad5abc1965d9abef
Aleksey Averianov <Flex_d1@mail.ru>
2025-06-05 17:08:42 +0300

    Разработка проекта

--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -1,11 +1,29 @@
 <script setup>
+  import Iconlocations from '../Icons/Iconlocations.vue';
   import Button from './Button.vue';
+  import Stat from './Stat.vue';
+
+
+  const data = {
+    label: 'Влажность',
+    stat: '90%',
+  }
+
 </script>
 
 <template>
   <div class="info-panel">
+    <div class="stat-wrapper">
+      <Stat :='data'></Stat>
+      <Stat label='Осадки' stat='0%'></Stat>
+      <Stat label='Ветер' stat='3 м/ч'></Stat>
+    </div>
     <Button>
-      Сохранить
+      <template #icon>
+        <Iconlocations />
+      </template>
+
+      Изменить город
     </Button>
   </div>
 </template>
@@ -13,5 +31,11 @@
 <style scoped>
   .info-panel {
     padding: 55px 50px 62px 479px;
+    width: 100%;
+  }
+  .stat-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
   }
 </style>
---
+++ src/components/Stat.vue
@@ -0,0 +1,32 @@
+<script setup>
+  const { label = "Не задано", stat="Нет информации" }  = defineProps({
+    label: String,
+    stat: String,
+  })
+</script>
+
+<template>
+  <div class="stat">
+    <div class="stat__title">{{ label }}</div>
+    <div class="stat__value">{{ stat }}</div>
+  </div>
+</template>
+
+<style scoped>
+  .stat {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    justify-content: space-between;
+  }
+  .stat__title {
+    font-size: 28px;
+    line-height: 100%;
+    font-weight: 700;
+  }
+  .stat__value {
+    font-size: 28px;
+    line-height: 100%;
+    font-weight: 500;
+  }
+</style>
